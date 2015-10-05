bb63022eaa12c65665d7bc705306b50617c7b2e1
lathspe11 <danjwilliams541@gmail.com>
2015-10-05 12:16:01 -0700

    base code creation
    Building a data importer for Bar separated file

---
+++ import_sql.py
@@ -0,0 +1,18 @@
+#SQL for importer
+
+#New Partition
+def createPartition(date)
+	pass
+
+#Does Partition exist?
+def ifPartition(date)
+	#Is date arg valid?
+	yr,mnth,day = date.split('-')
+	#Validate date has a partition
+
+	pass
+
+#Get partition name
+def getPartitionName(date)
+	pass
+	
---
+++ importer.py
@@ -0,0 +1,7 @@
+#python importer.py 
+
+errlog = 'C:\Users\djwilli\programs\logs\importerrors.txt'
+importFile = 'C:\Users\djwilli\programs\data\current.xls'
+
+#Partition Data 
+
