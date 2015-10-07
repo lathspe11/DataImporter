b5d491958fcaaadd7eb1c113533f86c96a829825
lathspe11 <danjwilliams541@gmail.com>
2015-10-06 22:38:49 -0700

    reader updated
    Code reads the file, needs to write to the partitions

--- import_sql.py
+++ import_sql.py
@@ -1,29 +1,56 @@
 #SQL for importer
 datapath = 'C:\\Users\\djwilli\\Documents\\Programs\\data\\'
 #New Partition
-def createMaster()
-	#STB|TITLE|PROVIDER|DATE|REV|VIEW_TIME
+import os
+import errno
 
-	"CREATE TABLE viewtrakmstr (
-    set_top_id      int not null,
-    view_title      char,
-    logdate         date not null,
-    revenue         float
-	)"
+def verifyDataFolder():
+	try :
+		os.makedirs(datapath)
+		yield true
 
-def createPartition(date)
-	createMaster()
-	pass
+	except OSError as exception:
+		if not os.path.isdir(datapath):
+			raise
+		yield false
+
+def verifyDataPath(mypath):
+	newpath = datapath + mypath
+	try :
+		os.makedirs(newpath)
+		yield true
+
+	except OSError as exception:
+		if not os.path.isdir(newpath):
+			raise
+		yield false
 
 #Does Partition exist?
-def ifPartition(date)
+def ifPartition(date):
 	#Is date arg valid?
 	yr,mnth,day = date.split('-')
 	#Validate date has a partition
-
-	pass
+	newpath = yr + "\\" + mnth
+	if verifyDataPath(yr):
+		print ("made dir " + yr)
+	else:
+		print (yr + "already exists")
+	if verifyDataPath(newpath):
+		print ("made dir " + newpath)
+	else:
+		print (newpath + "already exists")
+	yield datapath + newpath
 
 #Get partition name
-def getPartitionName(date)
-	pass
-	
+def getPartitionName(date):
+	#Is date arg valid?
+	yr,mnth,day = date.split('-')
+	#Validate date has a partition
+	newpath = yr + "\\" + mnth
+	yield newpath
+
+def writePartition(importDict,dtpath):
+	newpath = datapath + dtpath
+	partitionFile = open(newpath,"w")
+	for kval, dlist in importDict.iteritems():
+		pass
--- importer.py
+++ importer.py
@@ -1,39 +1,73 @@
 #python importer.py 
 import import_sql
 import csv 
-import six 
+import sys
+import os
 
-errlog = 'C:\Users\djwilli\programs\data\importerrors.txt'
-importFile = 'C:\Users\djwilli\programs\data\current.xls'
+errlog = 'C:\\Users\\djwilli\\programs\\data\\importerrors.txt'
+importFile = 'C:\\Users\\djwilli\\documents\\programs\\data\\initdata.txt'
 
 def usageMsg():
-	print "you're doing it wrong."
+	print("you're doing it wrong.")
 
-
-#do I have a file args to open?
-#If not open the test file importFile
-for arg in sys.argv[1:]
-	if isinstance(arg, six.string_types):
-		loadFile(arg)
-		latest = arg
-		#In a real system we would move the original file aside as history
-	else: 
-		loadFile(importFile)
-		latest = importFile
-
+def validFile(apath):
 	#if I can't open the file prnt usage
-	if not os.path.isfile(latest):
-		print usageMsg()
+	if not os.path.isfile(apath):
+		usageMsg()
+		print(apath + ' Not a valid path. Exiting')
 		sys.exit()
 	#
-	#Partition Data 
-	with open(latest, 'rb') as todayfile:
-		readOneLine = csv.reader(todayfile, delimiter='|',quotechar'"')
 
-		linedata = list()
+headers = list()
+linedata = list()
+partdict = {}
+vuedict = {}
 
-		 headers = next(readOneLine)
-		 linedata.append(line())
-		 
-		 for headers in cols:
-		 	linedata.append(line())
+def readImport(openme):
+	with open(openme, 'r') as todayfile:
+		readOneLine = csv.reader(todayfile, delimiter='|',quotechar='"')
+
+		#Strip off the header
+		headers = next(readOneLine)
+		print(headers[0:])
+		#Build lists for the keys
+		for col in headers:
+			linedata.append(list())
+			print(col)
+		rowcnt = 0
+
+		for aline in readOneLine:
+			key = aline[0] + '|' + aline[1] + '|' + aline[3]
+			yr,mnth,day = aline[3].split('-')
+			partnam = yr+"\\"+mnth
+			if partnam in partdict.keys():
+				partdict[partnam] += 1
+			else:
+				partdict[partnam] = 1
+			print(key + " %s" % ' '.join(str(x) for x in aline))
+			vuedict[key] = aline
+
+		print("number of partitions" + str(len(partdict)))
+		
+			
+#do I have a file args to open?
+#If not open the test file importFile
+print("This value will = 2 if in a command shell with an arg is given")
+cntargs = len(sys.argv)
+print(cntargs)
+
+if cntargs < 2 :
+	validFile(importFile)
+	latest = importFile
+	readImport(latest)
+else :
+	for arg in sys.argv[1:]:
+		if isinstance(arg, str) or isinstance(arg, unicode):
+			validFile(arg)
+			latest = arg
+			#In a real system we would move the original file aside as history
+			
+		#
+		#Partition Data 
+
+print('Program Completed')
