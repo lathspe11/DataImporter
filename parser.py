00e1528295c9687bc0210046f19f04798f6f7262
lathspe11 <danjwilliams541@gmail.com>
2015-10-09 00:00:39 -0700

    select and filter working
    Added argument parsing, select column processing, filter processing

--- parser.py
+++ parser.py
@@ -1,16 +1,76 @@
 import argparse
+import import_sql
 
-print(dir(argparse.ArgumentParser))
-argparse.ArgumentParser
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-s', action="store", dest="select", help='select string to use')
-parser.add_argument('-o', action="store", dest="order", help='order string to use')
-parser.add_argument('-f', action="store", dest="filter", help='filter string to use')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                   help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                   const=sum, default=max,
-                   help='sum the integers (default: find the max)')
+#print(dir(argparse.ArgumentParser))
+mydate = '2014-04-01'
+colDict = {'STB':0,'TITLE':1,'PROVIDER':2,'DATE':3,'REV':4,'VIEW_TIME':5}
+vuedict = {}
+vuelist = []
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+def selectcolum(colStr):
+	listCols = list()
+	transCols = list()
+	listCols = colStr.split(',')
+	for aCol in listCols:
+		ucCol = aCol.upper()
+		if ucCol in colDict:
+			transCols.append(colDict[ucCol]) 
+		else:
+			print("Did not find select string %s" % aCol)
+	return transCols
+
+#Filter data remove all but what matches the column value
+def filtercolum(colStr):
+	listCols = list()
+	transCols = list()
+	aCol,value = colStr.split('=')
+	ucCol = aCol.upper()
+	if ucCol in colDict:
+		tCol = colDict[ucCol]
+	else:
+		print("Did not find filter string %s" % aCol)
+	return tCol,value
+
+def main():
+	argparse.ArgumentParser
+	parser = argparse.ArgumentParser(description='Process some integers.')
+	parser.add_argument('-s', action="store", dest="select", 
+	help='select columns to display')
+	parser.add_argument('-o', action="store", dest="order", 
+	help='order by columns')
+	parser.add_argument('-f', action="store", dest="filter", help='filter data with a column value')
+	parser.add_argument('-g', action="store", dest="group", help='Group columns to display')
+
+	args = parser.parse_args()
+
+	#print(vars(args)) 
+	#Open the list 
+	apath = import_sql.getPartitionName(mydate)
+	vuedict = import_sql.openPartition(apath)
+	if args.filter != None:
+		print(args.filter)
+		aCol,aVal = filtercolum(args.filter)
+		print(aCol , aVal)
+		for v in vuedict.values():
+			if v[aCol] == aVal:
+				vuelist.append(v)
+	else:
+		for v in vuedict.values():
+			vuelist.append(v)
+	
+	#select disply columns
+	if args.select != None:
+		clist = sorted(selectcolum(args.select))
+		newlist = list()
+		for v in vuelist:
+			showlist = list()
+			for c in clist:
+				showlist.append(v[c])
+			newlist.append(showlist)
+		print(newlist)
+	#order columns
+	if args.order != None:
+		print(selectcolum(args.order))
+
+	
+main()
